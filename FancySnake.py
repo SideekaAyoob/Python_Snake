4e58b302bfaa34d3f6a3d24c559591d0ba689e7e
sideeka <136692184+SideekaAyoob@users.noreply.github.com>
2024-03-05 18:50:56 +0200

    Finished
    The production of the Fancy snake games is complete

--- FancySnake.py
+++ FancySnake.py
@@ -7,14 +7,31 @@ import random
 #define constants 
 WIDTH = 800
 HEIGHT = 600
+SNAKE_SIZE = 20
 DELAY = 100  # Milliseconds between screen updates
 offsets = {
-    "up":(0,20),
-    "down":(0,-20),
-    "left":(-20,0),
-    "right":(20,0)
+    "up":(0,SNAKE_SIZE),
+    "down":(0,-SNAKE_SIZE),
+    "left":(-SNAKE_SIZE,0),
+    "right":(SNAKE_SIZE,0)
 }
 FOOD_SIZE = 20
+high_score = 0 # High score
+
+# Load the high score if it exists
+try:
+    with open("high_score.txt", "r") as file:
+        high_score = int(file.read())
+except FileNotFoundError:
+    pass
+
+def update_high_score():
+    global high_score
+    if score > high_score:
+        high_score = score
+        with open("high_score.txt", "w") as file:
+            file.write("The Highest Score "+str(high_score))
+
 
 def bind_direction_keys():
     screen.onkey(lambda: set_snake_direction("up"), "Up")
@@ -39,7 +56,6 @@ def set_snake_direction(direction):
             snake_direction = "left"
 # end set_snake_direction
 
-
 #function for tutrle to move
 def game_loop():
     my_snake.clearstamps()
@@ -61,7 +77,14 @@ def game_loop():
             snake.pop(0)  # Keep the snake the same length unless fed.
 
         # Draw snake
-        for segment in snake:
+        my_snake.shape("assets/snake-head-20x20.gif")
+        my_snake.goto(snake[-1][0],snake[-1][1])
+        my_snake.stamp()
+        my_snake.shape("circle")
+    
+    #using list licing to ommit 1 bead of the 
+        #snake that is now being replaced by the snake head
+        for segment in snake[:-1]:
             my_snake.goto(segment[0], segment[1])
             my_snake.stamp()
 
@@ -77,6 +100,7 @@ def food_collision():
     global food_pos, score
     if get_distance(snake[-1], food_pos) < 20:
         score +=1
+        update_high_score()
         food_pos = get_random_food_pos()
         food.goto(food_pos)
         return True
@@ -110,7 +134,10 @@ def reset():
 screen = turtle.Screen()
 screen.setup(WIDTH,HEIGHT)
 screen.title("snake")
-screen.bgcolor("white")
+screen.bgpic("assets/bg2.gif")
+screen.register_shape("assets/snake-food-32x32.gif")
+screen.register_shape("assets/snake-head-20x20.gif")
+
 screen.tracer(0)
 
 #event handler
@@ -119,17 +146,16 @@ bind_direction_keys()
 
 my_snake = turtle.Turtle("circle")
 my_snake.penup()
-my_snake.color("green")
+my_snake.color("#009ef1")
 
 #create snake list as a list of co ordinaes
-snake = [[0,0], [20,0],[40,0],[60,0],[80,0]]
+snake = [[0,0], [SNAKE_SIZE,0],[SNAKE_SIZE*2,0],[SNAKE_SIZE*3,0]]
 snake_direction = "up"
 score = 0
 
 # Food
 food = turtle.Turtle()
-food.shape("triangle")
-food.color("red")
+food.shape("assets/snake-food-32x32.gif")
 food.shapesize(FOOD_SIZE / 20)  # Default size of turtle "square" shape is 20.
 food.penup()
 food_pos = get_random_food_pos()

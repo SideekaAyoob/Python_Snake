fe2720a2012b4cdcc17360b320b2080971d61e3a
sideeka <136692184+SideekaAyoob@users.noreply.github.com>
2024-03-05 15:54:34 +0200

    fourth
    adding food and scoring for the food

--- chap2.py
+++ chap2.py
@@ -1,15 +1,20 @@
+#https://github.com/SideekaAyoob/Python_Snake
+#python chap2.py
+
 import turtle
+import random
 
 #define constants 
 WIDTH = 500
 HEIGHT = 500
-DELAY = 400  # Milliseconds between screen updates
+DELAY = 350  # Milliseconds between screen updates
 offsets = {
     "up":(0,20),
     "down":(0,-20),
     "left":(-20,0),
     "right":(20,0)
 }
+FOOD_SIZE = 10
 
 def go_up():
     global snake_direction
@@ -34,27 +39,59 @@ def go_left():
 
 
 #function for tutrle to move
-def move_snake():
+def game_loop():
     my_snake.clearstamps()
+
     new_head = snake[-1].copy()
     new_head[0] += offsets[snake_direction][0]
     new_head[1] += offsets[snake_direction][1]
+   
+    # check for collisions
+    if new_head in snake or new_head[0]<-WIDTH/2 or new_head[0] >WIDTH /2\
+    or new_head[1] <- HEIGHT/2 or new_head[1] >HEIGHT/2:
+        turtle.bye()
+    else:
+        # No collision so we can continue moving the snake.
+        snake.append(new_head)
 
+        # Check food collision
+        if not food_collision():
+            snake.pop(0)  # Keep the snake the same length unless fed.
 
-    # add new head to snake
-    snake.append(new_head)
+        # Draw snake
+        for segment in snake:
+            my_snake.goto(segment[0], segment[1])
+            my_snake.stamp()
 
-    #remove last segment of snake
-    snake.pop(0)
+        # Refresh screen
+            screen.title(f"snake game. Score: {score}")
+        screen.update()
 
-    for segment in snake:
-        my_snake.goto(segment[0],segment[1])
-        my_snake.stamp()
-    #refresh screen
-    screen.update()
-    #rinse and repeat
-    turtle.ontimer(move_snake,DELAY)
+        # Rinse and repeat
+        turtle.ontimer(game_loop, DELAY)
 # end move _turtle
+
+def food_collision():
+    global food_pos, score
+    if get_distance(snake[-1], food_pos) < 20:
+        score +=1
+        food_pos = get_random_food_pos()
+        food.goto(food_pos)
+        return True
+    return False
+# end food collisions
+
+def get_random_food_pos():
+    x = random.randint(- WIDTH / 2 + FOOD_SIZE, WIDTH / 2 - FOOD_SIZE)
+    y = random.randint(- HEIGHT / 2 + FOOD_SIZE, HEIGHT / 2 - FOOD_SIZE)
+    return (x, y)
+# end get random food pos
+
+def get_distance(pos1, pos2):
+    x1,y1 = pos1
+    x2,y2= pos2
+    distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5  # Pythagoras
+    return distance
 
 #cereate a window where we will do our drawing
 screen = turtle.Screen()
@@ -74,16 +111,26 @@ my_snake = turtle.Turtle("square")
 my_snake.penup()
 
 #create snake list as a list of co ordinaes
-snake = [[0,0], [20,0],[40,0],[60,0]]
+snake = [[0,0], [20,0],[40,0],[60,0],[80,0]]
 snake_direction = "up"
+score = 0
 
 #draw a snake
 for segment in snake:
     my_snake.goto(segment[0],segment[1])
     my_snake.stamp()
 
+# Food
+food = turtle.Turtle()
+food.shape("circle")
+food.color("red")
+food.shapesize(FOOD_SIZE / 20)  # Default size of turtle "square" shape is 20.
+food.penup()
+food_pos = get_random_food_pos()
+food.goto(food_pos)
+
 #set animation
-move_snake()
+game_loop()
 
 # Finish nicely
 turtle.done()
